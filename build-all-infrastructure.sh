cbf7e395955f18df3dd6092087caa682aa16ee4b
psachdev6375 <puneet.sachdev@gmail.com>
2017-04-01 10:01:44 +0530

    Prefer IP Address

--- build-all-infrastructure.sh
+++ build-all-infrastructure.sh
@@ -1,3 +1,3 @@
 mvn --file config-server/pom.xml clean install
-mvn --file eureka-server/pom.xml clean install
+mvn --file eureka-server/pom.xml clean install -DskipTests
 mvn --file zuul-proxy/pom.xml clean install
